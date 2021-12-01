2189cfa0af899636f2643293a84a36f33f800235
Ali Sal <ali@moveaheadmedia.co.uk>
2021-12-01 13:55:40 +0700

    Initial commit

---
+++ mam-product-category-h1.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: MAM Product Category H1
+ * Plugin URI: https://github.com/keygen-sh/example-wordpress-plugin
+ * Description: This is an example WP plugin that utilizes Keygen for licensing.
+ * Version: 1.0.0
+ * Author: Keygen
+ * Author URI: https://keygen.sh
+ * License: GPL
+ */
