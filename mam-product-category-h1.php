5f19ef9a5118840c1c6e2f01bca96e9f3528b7cb
Ali Sal <ali@moveaheadmedia.co.uk>
2021-12-01 13:58:43 +0700

    Added shortcode [mam-product-category-h1] test

--- mam-product-category-h1.php
+++ mam-product-category-h1.php
@@ -2,10 +2,16 @@
 
 /**
  * Plugin Name: MAM Product Category H1
- * Plugin URI: https://github.com/keygen-sh/example-wordpress-plugin
- * Description: This is an example WP plugin that utilizes Keygen for licensing.
+ * Plugin URI: https://github.com/moveaheadmedia/mam-product-category-h1/
+ * Description: Use shortcode [mam-product-category-h1] to create custom H1 with Woof plugin.
  * Version: 1.0.0
- * Author: Keygen
- * Author URI: https://keygen.sh
+ * Author: Ali
+ * Author URI: https://github.com/moveaheadmedia/
  * License: GPL
  */
+
+//[mam-product-category-h1]
+function mam_product_category_h1( $atts ){
+    return "test";
+}
+add_shortcode( 'mam-product-category-h1', 'mam_product_category_h1' );
